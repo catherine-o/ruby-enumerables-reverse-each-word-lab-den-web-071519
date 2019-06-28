ff31467054c22a57dc0ad9b087742b037f8ff204
Catherine O'Hara <catherine.ohara@outlook.com>
2019-06-28 22:13:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   array = sentence.split(" ")
   new_array = []
-  array.collect do |word|
+  array.each do |word|
     new_array << word.reverse
   end
   new_array.join(" ")
